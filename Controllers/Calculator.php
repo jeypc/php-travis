64bea1dd7101f42b9ca7718438bcd8fc5eb66199
Jay <jeysean3@gmail.com>
2020-06-12 19:12:27 +0700

    Initial commit

---
+++ Controllers/Calculator.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Controllers;
+
+use InvalidArgumentException;
+
+class Calculator
+{
+    public function add($a, $b)
+    {
+        if ( ! is_int($a) or ! is_int($b) ) {
+            throw new InvalidArgumentException("Please enter a numeric value");
+        }
+
+        return $a + $b;
+    }
+}
---
+++ tests/CalculatorTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use Controllers\Calculator;
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    private $calc = null;
+
+    public function setUp(): void
+    {   
+        $this->calc = new Calculator;
+    }
+
+    public function tearDown(): void
+    {
+        $this->calc = null;
+    }
+
+    /**
+     * Test instance of $this->calc
+     * 
+     * @tests
+     */
+    public function testIntanceOf()
+    {
+        $this->assertInstanceOf('Controllers\Calculator', $this->calc);
+    }
+
+    /**
+     * Test provide invalid exception
+     * 
+     * @tests
+     */
+    public function testInvalidArgumentIsProperlyThrown()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->calc->add('A', 2);
+    }
+
+    /**
+     * Test is sum valid
+     * 
+     * @tests
+     */
+    public function testValidResult()
+    {
+        $this->assertEquals(5, $this->calc->add(1, 4));
+    }
+}
